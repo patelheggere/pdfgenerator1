ae1c2089c8f324e8bf16044a8727f7741a342624
patelheggere@gmail.com <patelheggere@gmail.com>
2018-01-12 17:01:22 +0530

    Initial commit

---
+++ app/src/main/java/com/patelheggere/pdfgenerator/MainActivity.java
@@ -0,0 +1,173 @@
+package com.patelheggere.pdfgenerator;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private Button mCreateBtn;
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initialize();
+        verifyStoragePermissions(MainActivity.this);
+    }
+
+    private void initialize()
+    {
+        textView = findViewById(R.id.tvText);
+        mCreateBtn = findViewById(R.id.createpdf);
+        mCreateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("created:"+write("sampl","dffgg"));
+                //createandDisplayPdf();
+                String pf = "/sdcard/sampl"+".pdf";
+                viewPdf(pf,"");
+            }
+        });
+    }
+
+    public void createandDisplayPdf()
+    {
+        Document doc = new Document();
+        try {
+        String path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS) + "/Dir";
+            File dir = new File(path);
+            if(!dir.exists())
+                dir.mkdirs();
+
+            File file = new File(dir, "newFile.pdf");
+            if(!file.exists())
+                file.createNewFile();
+
+            FileOutputStream fout = new FileOutputStream(file);
+            PdfWriter.getInstance(doc, fout);
+            doc.open();
+            Paragraph paragraph = new Paragraph(textView.getText().toString());
+            Font paraFont = new Font(Font.FontFamily.COURIER);
+            paragraph.setAlignment(Paragraph.ALIGN_CENTER);
+            paragraph.setFont(paraFont);
+            doc.add(paragraph);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            doc.close();
+        }
+        viewPdf("new2.pdf", "Dir");
+    }
+
+    // Method for opening a pdf file
+    private void viewPdf(String file, String directory) {
+
+        File pdfFile = new File(file);//new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS) + "/" + directory + "/" + file);
+        System.out.println(pdfFile.toString());
+        Uri path = Uri.fromFile(pdfFile);
+
+        // Setting the intent for pdf reader
+        Intent pdfIntent = new Intent(Intent.ACTION_VIEW);
+        pdfIntent.setDataAndType(path, "application/pdf");
+        pdfIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        try {
+            startActivity(pdfIntent);
+        } catch (ActivityNotFoundException e) {
+            Toast.makeText(MainActivity.this, "Can't read pdf file", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public boolean write(String fname, String fcontent) {
+        try {
+            //Create file path for Pdf
+            String fpath = "/sdcard/" + fname + ".pdf";
+            File file = new File(fpath);
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            // To customise the text of the pdf
+            // we can use FontFamily
+            Font bfBold12 = new Font(Font.FontFamily.TIMES_ROMAN,
+                    12, Font.BOLD, new BaseColor(0, 0, 0));
+            Font bf12 = new Font(Font.FontFamily.TIMES_ROMAN,
+                    12);
+            // create an instance of itext document
+            Document document = new Document();
+            PdfWriter.getInstance(document,
+                    new FileOutputStream(file.getAbsoluteFile()));
+            document.open();
+            //using add method in document to insert a paragraph
+            document.add(new Paragraph(textView.getText().toString()));
+
+            document.add(new Paragraph("Hello Veerendra Patel"));
+            document.addHeader("Header", "Header Content");
+            document.addTitle("Title");
+            document.addSubject("Subject");
+
+            // close document
+            document.close();
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        } catch (DocumentException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+
+    //persmission method.
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have read or write permission
+        int writePermission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int readPermission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.READ_EXTERNAL_STORAGE);
+
+        if (writePermission != PackageManager.PERMISSION_GRANTED || readPermission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }
+}
